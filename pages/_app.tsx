464aed6c1d040f05d972a5d111bcee7bafc32037
Imanshu Rathore <imanshurathore@gmail.com>
2022-06-28 17:13:06 +0530

    feat: inserted meta info of project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"
+        />
+        <title>imanshu rathore&apos;s</title>
+        <meta name="description" content="imanshu rathore's portfolio" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="imanshu rathore's portfolio"
+          key="og:title"
+        />
+        <meta
+          property="og:description"
+          content="Imanshu Rathore's Portfolio"
+          key="og:description"
+        />
+        <link rel="icon" href="/coding.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
