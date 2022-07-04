9db033993cd3178d62862074987687707d95b7b6
Imanshu Rathore <imanshurathore@gmail.com>
2022-07-05 04:03:07 +0530

    fix: moved timeline to commons folder

---
+++ Components/Common/Timeline.tsx
@@ -0,0 +1,56 @@
+import { Power2 } from 'gsap';
+import React, { useRef, useEffect, memo } from 'react';
+
+interface IProps {
+  isLast?: boolean;
+  timeline: GSAPTimeline | null;
+  index: number;
+}
+
+const Timeline: React.FC<IProps> = (props) => {
+  const { isLast, timeline, index } = props;
+  const dotRef = useRef(null);
+  const lineRef = useRef(null);
+
+  useEffect(() => {
+    if (timeline) {
+      timeline.addLabel('showTimeline', (index + 1) * 0.4);
+      timeline.fromTo(
+        dotRef.current,
+        {
+          opacity: 0,
+          ease: Power2.easeOut,
+        },
+        { opacity: 1 },
+        'showTimeline',
+      );
+      timeline.fromTo(
+        lineRef.current,
+        {
+          height: 0,
+          ease: Power2.easeOut,
+        },
+        { height: 'calc(100% - 0.8rem)' },
+        'showTimeline+=0.1',
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeline]);
+
+  return (
+    <div className="mr-2 md:mr-4 relative w-[2rem]">
+      <div
+        className="w-[0.4rem] h-[0.4rem] bg-transparent border-4 border-AteneoBlue dark:border-PastelPink absolute z-10 box-content rounded-[50%]"
+        ref={dotRef}
+      />
+      {!isLast && (
+        <div
+          className="w-[0.2rem] bg-WildBlue dark:bg-DeepRuby absolute left-[0.35rem] top-[0.8rem] h-0"
+          ref={lineRef}
+        />
+      )}
+    </div>
+  );
+};
+
+export default memo(Timeline);
